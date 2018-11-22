98b20c11381b7cd03df7f7b977fce0cf4e302f8a
Wim Verwaerde <wim.verwaerde@student.howest.be>
2018-11-22 15:17:06 +0100

    first changes

--- michiel.py
+++ michiel.py
@@ -1,33 +1,3 @@
-class Alert(object):
-    id = ""
-    title = ""
-    priority = 0
-    classification = ""
-    date=""
-    srcIp=""
-    srcPort=""
-    destIp=""
-    destPort=""
-    
-    # michiel
-
-    Protocol=""
-    TTL=""
-    TOS=""
-    PackedID=""
-    IpLen=0
-    DgmLen=0
-    Seq=""
-    Ack=""
-    Win=""
-    TcpLen=0
-
-    def __init__():
-        
-                
-    
-
-
 import os
 
 homeDir = os.environ['HOME']
@@ -35,11 +5,54 @@ snortAlertLocation = homeDir + '/Documents/Zeektrhusnortlogs/eternalblue_output/
 snortAlertFile = open(snortAlertLocation,"r")
 
 def readAlertFile(alertFile):
+
+    counter = 0
+    alert= {}
     for line in alertFile.readlines():
-         if len(line.strip()) != 0:
-         #
+        words = line.split()
+        if len(line.strip()) != 0:
+            if counter == 0:
+                nummerTussenBrackets = line.split('[', 1)[1].split(']')[1].split(' [')[1]
+                titelEersteLijn = line.split('] ')[2].split(' [')[0]
+                alert['weirdnumber']=nummerTussenBrackets
+                alert['title']=titelEersteLijn
+            if counter == 1:
+                val = line.split('[', 1)[1].split(']')[0]
+                valPriority = line.split('[', 1)[1].split(']')[1].split("[")[1]
+                if ":" in valPriority:  
+                    alert['priority']=valPriority.split(": ")[1] 
+                if ":" in val:  
+                    alert['classification']=val.split(": ")[1] 
+            if counter == 2:
+                for x in range(0, len(words)):
+                    lol = line.split(None, x + 1)[x]
+
+                    if "->" not in lol:
+
+                        if x == 0:                        
+                            alert['date'] = line.split(" ")[x]
+                        elif x == 1:           
+                            alert['srcPort'] = lol.split(":")[1]
+                            alert['srcIp'] = lol.split(":")[0]
+                        elif x == 3:
+                            alert['destPort'] = lol.split(":")[1]
+                            alert['destIp'] = lol.split(":")[0]
+
+            if counter == 3:  # als het de 3e lijn is moet je dit doen
+                for x in range(0, len(words)):
+                    lol = line.split(None, x + 1)[x]
+                    if ":" in lol:  # als er geen : staat moet je dit doen
+                        
+                        alert[lol.split(":")[0]] = lol.split(":")[1]
+                        
+                    #else:  # voor protecol
+
+            counter += 1
         else:
-            #hier begin je een nieuw object
-            break
+            # hier begin je een nieuw object
+            counter=0
+            
 
 readAlertFile(snortAlertFile)
+
+
---
+++ readbro.py
@@ -0,0 +1,30 @@
+import os
+
+homeDir = os.environ['HOME']
+BroLogLocation = homeDir + '/Documents/Zeektrhusnortlogs/eternalblue_output/bro/'
+
+def readBroLog(log):
+    logFields = []
+    broPackets = []
+    for line in log:
+        broPacket = {}
+        if line.startswith("#fields"):
+            logFields = line.split('\t')
+            del logFields[0]
+            logFields[len(logFields) -1] = logFields[len(logFields) -1].strip("\n") 
+        if not(line.startswith("#")):
+            logValues = line.split('\t')
+            for i in range(0, (len(logFields) -1)) :
+                broPacket[logFields[i]] = logValues[i]
+            broPackets.append(broPacket)
+    return (broPackets)
+                        
+def readBroLogs(broLogsLocation):
+    broLogsAsObjects = {}
+    for file in os.listdir(broLogsLocation):
+        if file.endswith(".log"):
+            with open(broLogsLocation + "/" + file) as f:
+                broLogsAsObjects[(file.strip('.log'))] = (readBroLog(f))
+    return broLogsAsObjects
+            
+print(readBroLogs(BroLogLocation))
